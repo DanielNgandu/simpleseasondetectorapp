cf3f9f12afb2292e9edff9336a416f960bc56bac
Daniel Ng`andu <daniel_ngandu@outlook.com>
2020-08-23 08:08:12 +0200

    About done!

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,7 +2,19 @@
 import React from "react";
 
 const SeasonDisplay = (props) => {
-  return <div>Season Display Component</div>;
+  return (
+    <div>
+      <h2 className="ui header ui green label ui huge label">
+        Your Current Location
+      </h2>
+      <div class="ui horizontal divider">-</div>
+      <div className="ui horizontal label">Latitude:</div>
+      <span>{props.latitude}</span>
+      <br />
+      <div className="ui horizontal label">Longitude: </div>
+      <span>{props.longitude}</span>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //import custom components
-// import SeasonDisplay from "./SeasonDisplay";
+import SeasonDisplay from "./SeasonDisplay";
 
+//class based component
 class App extends React.Component {
+  //called before instance of component is created.
+  //Also init of State
+  constructor(props) {
+    super(props);
+
+    //our state
+    this.state = { latitude: null };
+    this.state = { longitude: null };
+  }
+
+  //React says we have to define a render method
   render() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
       (err) => console.log(err)
     );
 
     return (
-      <div className="container">
-        <div>Latitude:</div>
-        <div>Longitude:</div>
+      <div className="ui raised very padded text container segment">
+        <SeasonDisplay
+          latitude={this.state.latitude}
+          longitude={this.state.longitude}
+        />
       </div>
     );
   }
